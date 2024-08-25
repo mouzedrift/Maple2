2d5de8ed8574073abb979e49b8d7a8621680a908
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-08-26 01:18:38 +0200

    fall back to cmd if windows terminal isn't installed

--- dev.bat
+++ dev.bat
@@ -1,5 +1,13 @@
 @echo off
 
-wt -d "Maple2.Server.World" --title "World Server" dotnet run ; ^
-sp -d "Maple2.Server.Login" --title "Login Server" dotnet run ; ^
-sp -d "Maple2.Server.Web" --title "Web Server" dotnet run
+where /q wt
+
+if %errorlevel% == 0 (
+	wt -d "Maple2.Server.World" --title "World Server" dotnet run ; ^
+	sp -d "Maple2.Server.Login" --title "Login Server" dotnet run ; ^
+	sp -d "Maple2.Server.Web" --title "Web Server" dotnet run
+) else (
+	start cmd /k "cd "Maple2.Server.World" & title World Server & dotnet run"
+	start cmd /k "cd "Maple2.Server.Login" & title Login Server & dotnet run"
+	start cmd /k "cd "Maple2.Server.Web" & title Web Server & dotnet run"
+)
--- start.bat
+++ start.bat
@@ -1,6 +1,15 @@
 @echo off
 
-wt -d "Maple2.Server.World" --title "World Server" dotnet run ; ^
-nt -d "Maple2.Server.Login" --title "Login Server" dotnet run ; ^
-nt -d "Maple2.Server.Web" --title "Web Server" dotnet run ; ^
-nt -d "Maple2.Server.Game" --title "Game Server" dotnet run
+where /q wt
+
+if %errorlevel% == 0 (
+	wt -d "Maple2.Server.World" --title "World Server" dotnet run ; ^
+	nt -d "Maple2.Server.Login" --title "Login Server" dotnet run ; ^
+	nt -d "Maple2.Server.Web" --title "Web Server" dotnet run ; ^
+	nt -d "Maple2.Server.Game" --title "Game Server" dotnet run
+) else (
+	start cmd /k "cd "Maple2.Server.World" & title World Server & dotnet run"
+	start cmd /k "cd "Maple2.Server.Login" & title Login Server & dotnet run"
+	start cmd /k "cd "Maple2.Server.Web" & title Web Server & dotnet run"
+	start cmd /k "cd "Maple2.Server.Game" & title Game Server & dotnet run"
+)
